62628540b0d36d3d3c6e2e9e53d72a7d6924ba9a
Mike Z <thecodingdoc@gmail.com>
2023-08-11 10:13:30 -0500

    Main update with some planets

--- planetSim.py
+++ planetSim.py
@@ -7,13 +7,18 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Planet Simulation")
 
 WHITE = (255,255,255)
+YELLOW = (255,255,0)
+BLUE = (100, 149, 237)
+RED = (188, 39, 50)
+DARK_GREY = ( 80, 78, 81)
+
 
 class Planet:
     AU = 149.6e6 * 1000 # distance from earth to sun in meters
     G = 6.67428e-11
-    SCALE= 250 / AU # 1 AU = 100px
+    SCALE= 125 / AU # 1 AU = 100px
     TIMESTEP = 3600*24 # 1 day
-    
+
 
 
     def __init__ (self, x, y, radius, color, mass):
@@ -30,9 +35,32 @@ class Planet:
         self.x_vel = 0
         self.y_vel = 0
 
+    def draw(self, win):
+        x = self.x * self.SCALE + WIDTH/2
+        y = self.y * self.SCALE + HEIGHT/2
+        pygame.draw.circle(win, self.color, (x,y), self.radius)
+
+
 def main():
     run = True
     clock = pygame.time.Clock()
+
+    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun.sun = True
+
+    earth = Planet(-1 * Planet.AU, 0,  16, BLUE, 5.9742 * 10**24)
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 3.30 * 10**23)
+    venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
+    # jupiter
+    # saturn
+    # uranus
+    # neptune
+    # pluto
+
+
+    planets = [sun, earth, mars, mercury, venus]
+
     while run:
         clock.tick(60)
         # WIN.fill(WHITE)
@@ -40,6 +68,12 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
+        
+        for planet in planets:
+            planet.draw(WIN)
+
+        pygame.display.update()
+
     pygame.quit()
 
 main()
